fb00bf68f4b233ce3adbd6bd51215af53b89806c
Hunter <hunterzhang86@gmail.com>
2022-10-28 15:14:06 +0800

    nocalhost + jrebel

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class Controller {
+    @GetMapping("/ping")
+    public String test() {
+        return "pong-2";
+    }
+}
