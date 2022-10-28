f1b29b99fe7bc035a13acd61a7f11d4fbab9547b
Hunter <hunterzhang86@gmail.com>
2022-10-28 22:39:34 +0800

    commit

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 mvn clean install -Dmaven.test.skip
 docker build -t nocalhost-jrebel-demo:v0.0.1 .
+# 镜像仓库的地址请根据实际情况调整
 docker tag nocalhost-jrebel-demo:v0.0.1 example.com/demo/nocalhost-jrebel-demo:v0.0.1
 docker push example.com/demo/nocalhost-jrebel-demo:v0.0.1
